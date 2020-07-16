9063c6df4c187f7d0395cc94d40bc774915e20aa
AssumeSphericalSeal <george.doran@mac.com>
2020-07-16 17:18:53 +0100

    Hot ABR start

--- Plasma_code/Models/HotABR.py
+++ Plasma_code/Models/HotABR.py
@@ -15,59 +15,62 @@ def colour():
 def norm_J_current(alpha,Phi,mu):
     j = (alpha**2)*(mu/((4*np.pi)**0.5))*np.exp(-Phi)
     return(j)
-#Define equation ABR 13 for the boundary potential Phi_b.
+#Define equation ABR 13 for the boundary potential tau + z*Phi_b.
 def Eq_Phi_b(Phi_b, J, z=1,gamma = 10000):
-    return 4 * (Phi_b**(3/2))*(2*Phi_b-3)*(2*Phi_b+1) - J/(gamma*np.sqrt(z)) * ((2*Phi_b-1)**3)
+    return 4 * (Phi_b**(3/2))*(2*Phi_b-3*z)*(2*Phi_b+z) - J/(gamma) * ((2*Phi_b-z)**3)
 #Calculate the boundary conditions.
-def get_boundary(J,z,gamma):
-    Phi_b_initial_guess = 0.25
-    Phi_b_solution = fsolve(Eq_Phi_b, Phi_b_initial_guess, args = (J,z,gamma))
-    Phi_b = Phi_b_solution[0]
+def get_boundary(J,tau,z,gamma):
+    P_b_initial_guess = 0.25
+    P_b_solution = fsolve(Eq_Phi_b, P_b_initial_guess, args = (J,z,gamma))
+    P_b = P_b_solution[0]
+    Phi_b = (P_b-tau)/z
     #Calculate rho at the boundary. 
-    rho_b = (np.sqrt(J) * np.exp(Phi_b/2))/((Phi_b*z)**(1/4))
+    rho_b = (np.sqrt(J) * np.exp(Phi_b/2))/((P_b)**(1/4))
     #Calculate the first derivative of Phi with renpect to rho evaluated at the boundary.
-    dPhi_drho_b = ((2*rho_b/J) * (Phi_b**(3/2))/(Phi_b - 1/2) * np.exp(-Phi_b))*(z**0.5)
+    dPhi_drho_b = (((2*rho_b/J) * (P_b**(3/2))/(P_b - 0.5*z)) * np.exp(-Phi_b))*(z**0.5)
     return(rho_b,Phi_b,dPhi_drho_b)
 #Runge-Kutta 4th order ODE solver
-def ABR_f(x,y,z):
-    return z
-def ABR_g(x,y,z,A):
-    return -(2/x)*z + (A/(x**2))*(y**(-0.5)) - np.exp(-y)
-def ABR_RK(x0,y0,z0,X,A,N):
+def ABR_f(x,y,w):
+    return w
+def ABR_g(x,y,w,J,tau,z):
+    return -(2/x)*w + (J/(x**2))*(tau + z*(y**(-0.5))) - np.exp(-y)
+def ABR_RK(x0,y0,w0,X,J,tau,z,N):
     h = (X-x0)/N
     x = X - N*h
     y = y0
-    z = z0
+    w = w0
     for n in range(N):
-        k1 = ABR_f(x,y,z)
-        l1 = ABR_g(x,y,z,A)
+        k1 = ABR_f(x,y,w)
+        l1 = ABR_g(x,y,w,J,tau,z)
 
-        k2 = ABR_f(x+h/2,y+k1*h/2,z+l1*h/2)
-        l2 = ABR_g(x+h/2,y+k1*h/2,z+l1*h/2,A)
+        k2 = ABR_f(x+h/2,y+k1*h/2,w+l1*h/2)
+        l2 = ABR_g(x+h/2,y+k1*h/2,w+l1*h/2,J,tau,z)
 
-        k3 = ABR_f(x+h/2,y+k2*h/2,z+l2*h/2)
-        l3 = ABR_g(x+h/2,y+k2*h/2,z+l2*h/2,A)
+        k3 = ABR_f(x+h/2,y+k2*h/2,w+l2*h/2)
+        l3 = ABR_g(x+h/2,y+k2*h/2,w+l2*h/2,J,tau,z)
 
-        k4 = ABR_f(x+h,y+k3*h,z+l3*h)
-        l4 = ABR_g(x+h,y+k3*h,z+l3*h,A)
+        k4 = ABR_f(x+h,y+k3*h,w+l3*h)
+        l4 = ABR_g(x+h,y+k3*h,w+l3*h,J,tau,z)
 
         x += h
         y += 1/6 * (k1 + 2*k2 + 2*k3 + k4)*h
-        z += 1/6 * (l1 + 2*l2 + 2*l3 + l4)*h
+        w += 1/6 * (l1 + 2*l2 + 2*l3 + l4)*h
                 
     return(y0,y)
 
-def Phi_J(J,alpha,mu,z,gamma = 10000):
-    x0,y0,z0 = get_boundary(J,z,gamma) 
-    Phi_b,Phi_alpha = ABR_RK(x0,y0,z0,alpha,J/np.sqrt(z),N=10000)
+def Phi_J(J,alpha,mu,tau,z,gamma = 10000):
+    x0,y0,w0 = get_boundary(J,tau,z,gamma) 
+    Phi_b,Phi_alpha = ABR_RK(x0,y0,w0,alpha,J,tau,z,N=10000)
     return Phi_alpha
 
-def delta_J(J,alpha,mu,z,gamma = 10000):
-    return J - norm_J_current(alpha,Phi_J(J,alpha,mu,z,gamma = 10000),mu)
+def delta_J(J,alpha,mu,tau,z,gamma = 10000):
+    return J - norm_J_current(alpha,Phi_J(J,alpha,mu,tau,z,gamma = 10000),mu)
 def retrive_Phi_a(J,mu,alpha):
     return 2*np.log(alpha) + np.log(mu) - np.log(J) - 0.5*np.log(4*np.pi)
 
 def potential_finder(Theta,mu,z,alpha,upsilon,gamma=10000):
+    tau = (1/(4*np.pi))*Theta
+    #tau = 1.5*Theta
     #Guess Phi_a (Its likely to be between 0 and 10)
     if alpha != 0 :
         if alpha < 1e-5 and alpha > 0:
@@ -75,7 +78,7 @@ def potential_finder(Theta,mu,z,alpha,upsilon,gamma=10000):
         elif alpha > 1e12:
             Jsol = fsolve(delta_J,norm_J_current(alpha,-0.5*np.log(2*np.pi)+0.5+np.log(z*mu),mu),args = (alpha,mu,z,gamma))
         else:
-            Jsol = bisect(delta_J,norm_J_current(alpha,0,mu),norm_J_current(alpha,-0.5*np.log(2*np.pi)+0.5+np.log(z*mu),mu),args = (alpha,mu,z,gamma))
+            Jsol = bisect(delta_J,norm_J_current(alpha,0,mu),norm_J_current(alpha,-0.5*np.log(2*np.pi)+0.5+np.log(z*mu),mu),args = (alpha,mu,tau,z,gamma))
         return retrive_Phi_a(Jsol,mu,alpha)
     else:
         return 0 #As argued by Kennedy and Allen
@@ -92,4 +95,5 @@ def priority(Theta,alpha,upsilon):
         P_u = 1
     return (P_t + P_a + P_u)
 
+#potential_finder(Theta,mu,z,alpha,upsilon)
 print(potential_finder(0.01,43,1,5,0))
