7bd5013e8bc5c59094eff19cdb068f95bc544029
moleke51 <doganakpinar@hotmail.co.uk>
2020-07-23 15:39:18 +0100

    New OML

--- Plasma_code/Models/ABRFitOM.py
+++ Plasma_code/Models/ABRFitOM.py
@@ -2,20 +2,24 @@ import scipy as sp
 import numpy as np
 import matplotlib.pyplot as plt
 import HotABR as abr
-import scipy.optimize as spl
+import scipy.optimize as spo
 
 def function(alpha,kappa):
-    Theta = 0.01
+    Theta = 1
     mu = 43
     z = 1
     upsilon = 0
-    Phi = abr.potential_finder(Theta,mu,z,alpha,upsilon,kappa)
+    Phi_list = []
+    for i in range(len(alpha)):
+        Phi = abr.potential_finder(Theta,mu,z,alpha[i],upsilon,kappa)
+        Phi_list.append(Phi)
+    Phi = np.array(Phi_list)
     return Phi
 
-alpha,Phi = np.loadtxt("/Users/doganakpinar/Documents/Physics_Research/DustyPlasmaCollab/Plasma_code/Models/OMData1.txt",skiprows=1,unpack=True)
 
-Phi_fit, Phi_cov = spl.curve_fit(function,alpha,Phi)
-print(Phi_fit)
-#plt.plot(alpha,Phi_fit(alpha))
-#plt.grid()
-#plt.show()
+alpha,Phi = np.loadtxt("/Users/doganakpinar/Documents/Physics_Research/DustyPlasmaCollab/Plasma_code/Models/OMData3.txt",skiprows=1,unpack=True)
+Kappa, cov = spo.curve_fit(function,alpha,Phi,0.25)
+print(Kappa,cov)
+plt.plot(alpha,function(alpha,Kappa))
+plt.grid()
+plt.show()
