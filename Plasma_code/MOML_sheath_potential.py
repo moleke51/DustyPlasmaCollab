50bf32c988ed71f15f6b26ba9792b87963b51f9d
moleke51 <doganakpinar@hotmail.co.uk>
2020-07-30 14:22:18 +0100

    pushing

--- Plasma_code/MOML_sheath_potential.py
+++ Plasma_code/MOML_sheath_potential.py
@@ -2,7 +2,7 @@ import numpy as np
 import scipy as sp
 import scipy.special as sps
 import matplotlib.pyplot as plt 
-from scipy.optimize import fsolve
+import scipy.optimize as spo
 
 def realLambertW(x):
     if type(x) is float or type(x) is int or type(x) is np.float64 :
@@ -28,29 +28,42 @@ def sheath_pot(Theta,gamma): #gamma = 5/3
     Phi = Theta - realLambertW((np.sqrt(2*np.pi*Theta*(1+(gamma)*Theta)))*np.exp(Theta)) 
     return Phi #returned phi is positive     
 
-def presheath_drop(Theta,gamma):
-    return -1*0.5 + (-1*gamma + 3/2)*Theta
+#def presheath_drop(Theta,gamma):
+#    return -1*0.5 + (-1*gamma + 3/2)*Theta
 
-def RI(Theta,gamma = 5/3):
-    return (sheath_pot(Theta,gamma) - presheath_drop(Theta,gamma))
+#def RI(Theta,gamma = 5/3):
+#    return (sheath_pot(Theta,gamma) - presheath_drop(Theta,gamma))
+
+def fit(Theta,Phi,mu,z,gamma,A):
+    gamma = 5/3
+    mu = 43
+    z = 1
+    Phi_sheath = 0.5*np.log(2*np.pi*(1+gamma*Theta))-np.log(z/mu)
+    rho_s = (np.sqrt(2)/3)*(2*Phi_sheath)**(3/4)
+    return -A/rho_s
+    
 
 Theta = np.logspace(-9,2,1000)
-gamma = 5/3*np.ones(len(Theta))
+gamma = 5/3
+mu = 43
+z = 1
 Phi_s = sheath_pot(Theta,gamma)
-Phi_ps = presheath_drop(Theta,gamma)
+fit,cov = spo.curve_fit(fit,Theta,Phi_s,0.25)
+print(fit)
+#Phi_ps = presheath_drop(Theta,gamma)
 
-Theta1 = fsolve(RI,0)
-Theta2 = fsolve(RI,1e1)
-print(Theta1, Theta2)
+#Theta1 = fsolve(RI,0)
+#Theta2 = fsolve(RI,1e1)
+#print(Theta1, Theta2)
 
 plt.plot(Theta,Phi_s,color = 'Black',label = "MOML prediction")
-plt.plot(Theta,Phi_ps,'--',color = 'Red', label = "Derived prediction")
-plt.plot(Theta1,presheath_drop(Theta1,5/3),'x',color = "Blue", label = f"Theta1 = {Theta1}")
-plt.plot(Theta2,presheath_drop(Theta2,5/3),'x',color = "Blue", label = f"Theta2 = {Theta2}")
+#plt.plot(Theta,Phi_ps,'--',color = 'Red', label = "Derived prediction")
+#plt.plot(Theta1,presheath_drop(Theta1,5/3),'x',color = "Blue", label = f"Theta1 = {Theta1}")
+#plt.plot(Theta2,presheath_drop(Theta2,5/3),'x',color = "Blue", label = f"Theta2 = {Theta2}")
 plt.grid()
 plt.title('The potential drop across the presheath')
 plt.xlabel('$\Theta$')
 plt.ylabel('$\Phi$')
-plt.xscale('log')
+#plt.xscale('log')
 plt.legend()
 plt.show()
--- Plasma_code/Models/NewAttempt.py
+++
@@ -1,8 +0,0 @@
-import numpy as np
-import matplotlib.pyplot as plt 
-from scipy.optimize import fsolve,bisect
-
-
-
-
-
